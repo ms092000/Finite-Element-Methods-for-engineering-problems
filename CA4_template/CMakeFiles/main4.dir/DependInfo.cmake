bf6e08566ad436bc5a598ab8a600b96c71fea523
Manish Sahoo <manishiiti2019@gmail.com>
2025-04-13 02:28:33 +0900

    First Commit

---
+++ CA4_template/CMakeFiles/main4.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/Downloads/CA4_template/main4.cc" "/home/ubuntu/Downloads/CA4_template/CMakeFiles/main4.dir/main4.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_NO_AUTO_PTR"
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ubuntu/deal.II/installed/include"
+  "/home/ubuntu/deal.II/installed/include/deal.II/bundled"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include"
+  "/home/ubuntu/libs/parmetis-4.0.3/include"
+  "/home/ubuntu/libs/trilinos-release-12-18-1/include"
+  "/home/ubuntu/libs/petsc-3.13.1/include"
+  "/home/ubuntu/libs/hdf5-1.10.7/include"
+  "/home/ubuntu/libs/oce-OCE-0.18.3/include/oce"
+  "/home/ubuntu/libs/p4est-2.3.2/FAST/include"
+  "/home/ubuntu/libs/slepc-3.13.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
